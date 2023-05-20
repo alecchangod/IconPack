fdb37587be28e92ca493fc4cc073687348904d28
Alec Chan <alec3d02@gmail.com>
2023-05-20 18:07:45 +0800

    initial commit

---
+++ app/src/main/kotlin/acg/vtb/iconpack/app/MuzeiService.kt
@@ -0,0 +1,5 @@
+package acg.vtb.iconpack
+
+import dev.jahir.frames.muzei.FramesArtProvider
+
+class MuzeiService : FramesArtProvider()
--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -1,7 +1,7 @@
 @file:Suppress("unused")
 
 object MyApp {
-    const val appId = "dev.jahir.blueprint.app"
-    const val version = 234
-    const val versionName = "2.3.4"
+    const val appId = "acg.vtb.iconpack"
+    const val version = 1
+    const val versionName = "1.0"
 }
